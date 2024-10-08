24bbbd10b79e9d91b832456e281a5e0993249784
antho <amateod2@miumg.edu.gt>
2024-10-08 03:41:54 -0600

    Video 2 carpeta nueve, catalogos

--- bases/models.py
+++ bases/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class ClaseModelo(models.Model):  # Corregido 'codels.Model' a 'models.Model'
+
+    estado = models.BooleanField(default=True)  # 'True' en mayúscula
+    fc = models.DateTimeField(auto_now_add=True)  # 'True' en mayúscula
+    fm = models.DateTimeField(auto_now=True)  # 'True' en mayúscula
+    uc = models.ForeignKey(User, on_delete=models.CASCADE)
+    um = models.IntegerField(blank=True, null=True)  # 'True' en mayúscula
+
+    class Meta:
+        abstract = True  # 'True' en mayúscula
