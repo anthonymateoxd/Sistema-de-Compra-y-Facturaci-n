803593cef21500529df7c954918d239aa348dbfd
antho <amateod2@miumg.edu.gt>
2024-10-08 12:08:33 -0600

    Se agregaron cambios en todos los archivos, carpeta-9  video 1-8

--- bases/urls.py
+++ bases/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-
-from bases.views import Home  # Asegúrate de que esta importación sea correcta
+from .views import SomeView  # Importa la vista que necesitas
 
 urlpatterns = [
+     path('', SomeView.as_view(), name='some_view'),
     path('',include(('bases.urls','bases'), namespace='bases')),
     path('admin/', admin.site.urls)
 ]
--- inv/models.py
+++ inv/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+from bases.models import ClaseModelo  
+
+class Categoria(ClaseModelo):  
+    description = models.CharField(  
+        max_length=100,
+        help_text='Descripcion de la Categoria',
+        unique=True
+    )
+
+    def __str__(self):
+        return '{}'.format(self.description)
+
+    def save(self, *args, **kwargs): 
+        self.description = self.description.upper()
+        super(Categoria, self).save(*args, **kwargs)
+
+    class Meta:
+        verbose_name_plural = "Categorias"  
