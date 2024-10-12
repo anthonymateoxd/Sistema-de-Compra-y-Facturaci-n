5eb0e0084b2d3bf2419e5a1f4a424e7773ebabda
antho <amateod2@miumg.edu.gt>
2024-10-12 00:20:01 -0600

    cambios

--- bases/views.py
+++ bases/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.views import generic
 
 class Home(generic.TemplateView):
-    template_name = 'index.html'
+    template_name = 'home.html'
--- prueba/settings.py
+++ prueba/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'prueba.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS':[os.path.join(BASE_DIR,'Base'),],
+        'DIRS':[os.path.join(BASE_DIR,'templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,6 +119,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = '/Base/'
+STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR,'Base'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR,'templates'),)
