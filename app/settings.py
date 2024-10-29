d0d36952366c85c15ef7a72e1425332ce4983ed6
antho <amateod2@miumg.edu.gt>
2024-10-28 21:14:38 -0600

    Coreccion

--- app/settings.py
+++ app/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY=config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1:8000","herokuapp.com"]
+ALLOWED_HOSTS = ["lospanchoshermanos-4f17336a2b80.herokuapp.com", "127.0.0.1", ".herokuapp.com"]
 
 # Application definition
 
